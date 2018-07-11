feb170bd42ca6e643d4751da1f3f1f5184c98486
Andrey Borisenko <andrey.borisenko23@gmail.com>
2018-07-11 16:01:29 +0300

    Moved to nodemailer

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
 
-var send = require('gmail-send')({
+const credentials = {
     user: 'popfever.contact@gmail.com',
     pass: 'popfever123'
-})
+}
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: credentials.user,
+        pass: credentials.pass
+    }
+});
+
+// var send = require('gmail-send')({
+//     user: 'popfever.contact@gmail.com',
+//     pass: 'popfever123'
+// })
 
 const app = express()
 
@@ -27,15 +41,29 @@ app.post('/send', bodyParser.json({ type: 'application/*+json' }), (req, res) =>
 
     if (!Object.keys(req.body).length || !req.body.mail_to) return res.end('failed')
 
-    send({
-        to: req.body.mail_to,
-        subject: req.body.subject || 'Influencer',
-        html: req.body.message || 'No message'
-    })
+    const mailOptions = {
+        from: credentials.user, // sender address
+        to: req.body.mail_to, // list of receivers
+        subject: req.body.subject || 'Influencer', // Subject line
+        html: req.body.message || 'No message' // plain text body
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err)
+          console.log(err)
+        else
+          console.log(info);
+     });
+
+    // send({
+    //     to: req.body.mail_to,
+    //     subject: req.body.subject || 'Influencer',
+    //     html: req.body.message || 'No message'
+    // })
 
     res.writeHead(200)
     res.end('good')
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5555
 app.listen(port, () => console.log(`Server is running on port ${port}`))
